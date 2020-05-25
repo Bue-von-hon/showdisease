ef4039598a971c9f51ba62dfd98ecaf3c61491df
paradohee <paradohee0518@gmail.com>
2020-05-26 00:04:49 +0900

    일단 리스트 기본 모양새만 잡아놓은 상태입니다..(데이터는 일단 임의의 값을 주었고요) 그리고 편집액티비티에서 버튼 위치는 다시 수정하겠습니다...ㅠㅠ

---
+++ app/src/main/java/com/kc/showdisease/DiseaseList.java
@@ -0,0 +1,14 @@
+package com.kc.showdisease;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DiseaseList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disease_list);
+    }
+}
