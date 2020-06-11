781533221785c58a48205a7c56833f6913ed67f8
LinaKK <64690361+LinaKK@users.noreply.github.com>
2020-06-11 14:04:20 +0900

    #30, #31

--- app/src/main/java/com/kc/showdisease/Disease_Infos.java
+++ app/src/main/java/com/kc/showdisease/Disease_Infos.java
@@ -13,18 +13,28 @@ import android.widget.ArrayAdapter;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 
+import java.util.List;
 import java.util.ListIterator;
 
 import static com.kc.showdisease.MainActivity.db;
 
 public class Disease_Infos extends AppCompatActivity {
 
+    private ListView mList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disease__infos);
-        
 
+        mList = (ListView) findViewById(R.id.diseas_list);
+
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
 
         }
 
