0f5ec0807a66551835dfe32dac5258312dfa701b
LinaKK <64690361+LinaKK@users.noreply.github.com>
2020-06-11 13:55:07 +0900

    #30, #31

---
+++ app/src/main/java/com/kc/showdisease/Disease_Infos.java
@@ -0,0 +1,40 @@
+package com.kc.showdisease;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+
+import java.util.ListIterator;
+
+import static com.kc.showdisease.MainActivity.db;
+
+public class Disease_Infos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disease__infos);
+        
+
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
